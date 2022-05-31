c82434ff56efbf12384cbc2f0b4244a82a6f849e
Kevin Arifin <kevarifin14@gmail.com>
2022-05-31 00:53:00 -0700

    another header attempt

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -7,10 +7,19 @@ const handler = nextConnect<NextApiRequest, NextApiResponse>();
 
 handler.post(async (req, res, next) => {
   try {
+    const headers = {};
+    if (req.headers["x-hasura-role"]) {
+      headers["x-hasura-role"] = req.headers["x-hasura-role"];
+    }
+
+    if (req.headers["x-hasura-admin-secret"]) {
+      headers["x-hasura-admin-secret"] = req.headers["x-hasura-admin-secret"];
+    }
+
     const response = await post(
       process.env.HASURA_ENDPOINT!,
       req.body,
-      req.headers
+      headers
     );
     res.status(200).json(response);
   } catch (e) {
