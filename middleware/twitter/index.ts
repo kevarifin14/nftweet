62b3315522c38900d2179b91424ece8114862f47
Kevin Arifin <kevarifin14@gmail.com>
2022-06-07 15:50:18 -0700

    disable mentions for now

--- middleware/twitter/index.ts
+++ middleware/twitter/index.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextHandler } from "next-connect";
 import { Client } from "twitter-api-sdk";
 
-// export const twitterClient = new Client(process.env.TWITTER_BEARER_TOKEN!);
-export const twitterClient = new Client(process.env.TWITTER_RW_ACCESS_TOKEN!);
+export const twitterClient = new Client(process.env.TWITTER_BEARER_TOKEN!);
+// export const twitterClient = new Client(process.env.TWITTER_RW_ACCESS_TOKEN!);
 
 export interface TwitterApiRequest extends NextApiRequest {
   twitterClient: Client;
--- pages/api/mentions.ts
+++ pages/api/mentions.ts
@@ -25,52 +25,56 @@ handler.post(async (req, res, next) => {
     const adminApollo = createAdminApolloClient();
     const date = new Date();
     const startTime = format(
-      sub(addMinutes(date, date.getTimezoneOffset()), { minutes: 15 }),
+      sub(addMinutes(date, date.getTimezoneOffset()), { minutes: 30 }),
       "yyyy-MM-dd'T'HH:mm:ss'Z'"
     );
 
     const value = await req.twitterClient.tweets.usersIdMentions(
       "1529641301724909568",
-      { start_time: startTime, "tweet.fields": ["referenced_tweets"] }
+      {
+        start_time: startTime,
+        "tweet.fields": ["referenced_tweets", "in_reply_to_user_id"],
+      }
     );
 
-    try {
-      if (value.data) {
-        await adminApollo.mutate<
-          AddMentionsMutation,
-          AddMentionsMutationVariables
-        >({
-          mutation: AddMentionsDocument,
-          variables: {
-            objects: value.data?.map((tweet) => ({ tweetId: tweet.id })),
-          },
-        });
+    // try {
+    //   if (value.data) {
+    //     await adminApollo.mutate<
+    //       AddMentionsMutation,
+    //       AddMentionsMutationVariables
+    //     >({
+    //       mutation: AddMentionsDocument,
+    //       variables: {
+    //         objects: value.data?.map((tweet) => ({ tweetId: tweet.id })),
+    //       },
+    //     });
 
-        await Promise.all(
-          value.data.map((mention) => {
-            const tweetToReplyTo =
-              mention.referenced_tweets?.find(
-                ({ type }) => type === "replied_to"
-              ) ||
-              mention.referenced_tweets?.find(
-                ({ type }) => type === "quoted"
-              ) ||
-              mention;
-            return req.twitterClient.tweets.createTweet({
-              text: `Mint your NFTweet👇\n\nhttps://makenftweet.com/?tweetId=${tweetToReplyTo.id}`,
-              reply: { in_reply_to_tweet_id: tweetToReplyTo.id },
-            });
-          })
-        );
-      }
-      res.status(200).json(value);
-    } catch (e) {
-      console.log(e);
-    }
+    //     await Promise.all(
+    //       value.data.map((mention) => {
+    //         const tweetToReplyTo =
+    //           mention.referenced_tweets?.find(
+    //             ({ type }) => type === "replied_to"
+    //           ) ||
+    //           mention.referenced_tweets?.find(
+    //             ({ type }) => type === "quoted"
+    //           ) ||
+    //           mention;
+    //         return req.twitterClient.tweets.createTweet({
+    //           text: `Mint your NFTweet👇\n\nhttps://makenftweet.com/?tweetId=${tweetToReplyTo.id}`,
+    //           reply: { in_reply_to_tweet_id: tweetToReplyTo.id },
+    //         });
+    //       })
+    //     );
+    //   }
+    //   res.status(200).json(value);
+    // } catch (e) {
+    //   console.log(e);
+    // }
   } catch (e) {
     console.log(e);
     res.status(400).json(e);
   }
+
   return next();
 });
 
