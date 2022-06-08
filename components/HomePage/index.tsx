248e9771fab160ff954a28713a8f7793f451649c
Kevin Arifin <kevarifin14@gmail.com>
2022-06-07 18:46:12 -0700

    clean up home page

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -1,15 +1,9 @@
-import { Nft } from "@metaplex-foundation/js-next";
-import { PublicKey } from "@solana/web3.js";
 import { useNftweetsQuery } from "generated";
-import { useMetaplex } from "hooks/useMetaplex";
 import _ from "lodash";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 import { Container } from "components/Container";
 import { Header } from "components/Header";
-import { Input } from "components/Input";
 import { LoadingPage } from "components/Layout/LoadingPage";
 
 import { NftweetCard } from "./NftweetCard";
